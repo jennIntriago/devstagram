639c474bfce7788cd12d9799c9261a0ed23b94a2
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-05-15 16:39:12 -0500

    Conexion con la base de datos e interfaz de registro de usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('principal');
 });
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
